452f0da3d3483b4f23d79cc0180409f9f1d59ca2
dr1s <dr1s@users.noreply.github.com>
2022-05-09 15:07:13 +0700

    ncdu: remove HEAD build, doesnt work anyway

--- ncdu.rb
+++ ncdu.rb
@@ -5,17 +5,9 @@ class Ncdu < Formula
   sha256 "810745a8ed1ab3788c87d3aea4cc1a14edf6ee226f764bcc383e024ba56adbf1"
   license "MIT"
 
-  head do
-    url "https://g.blicky.net/ncdu.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
-
   uses_from_macos "ncurses"
 
   def install
-    system "autoreconf", "-i" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
